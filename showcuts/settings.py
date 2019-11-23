1b4f29c369b02dbfca22f8fb1197188b527b88ee
Elijah <emlyap99@gmail.com>
2019-11-23 16:40:11 +0800

    Added allowed hosts, turned off debug

--- showcuts/settings.py
+++ showcuts/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bnphip=1c_tmjei*$%qk4a2v4(o8+svyokq!9e7m!$d0ak(xw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS += ip_addresses()
+ALLOWED_HOSTS = ['167.172.247.158']
 
 # Application definition
 
