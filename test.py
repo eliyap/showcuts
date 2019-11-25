3b75536a3f94702f6cc9d3da19bd8a8f0abb91e0
Elijah <emlyap99@gmail.com>
2019-11-25 03:52:37 +0000

    remove duplication in gitignore

---
+++ test.py
@@ -0,0 +1,6 @@
+# test.py
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World"] # python3
+    #return ["Hello World"] # python2
+
