264e01a0d58d9119b3f6fac05dd66f387a0ff373
Elijah <emlyap99@gmail.com>
2019-11-28 18:18:47 +0800

    add battery tests for apple notes and date action

---
+++ share/tests/test_batteries/test_apple_notes.py
@@ -0,0 +1,88 @@
+## Dependency: sys
+from unittest import TestCase
+import logging
+
+
+## Dependency: battery testing
+from share.tests.test_batteries.battery import *
+
+
+class test_apple_notes(TestCase):
+    reference_image = ''
+    actionID = '1c64f0b1cd3246c098c552f16b5169da'
+
+    # boilerplate
+
+    def setUp(self):
+        self.actions = actions_from_ID(__class__.actionID)
+
+    def tearDown(self):
+        del self.actions
+
+    def test_all_actions_loaded(self):
+        test_for_errors(self)
+        test_no_not_implemented(self)
+
+
+    # Tailored Tests
+
+    def test_glyphs_correct(self):
+        [test_correct_glyph(self, action, 'Notes.png') for action in self.actions]
+
+    def test_categoryies_correct(self):
+        [self.assertEqual(action['category'], 'NOTES') for action in self.actions]
+
+
+    def test_titles_correct(self):
+        # Append X to Y
+        compare_titles(self, 0, [
+            {'class':'', 'value':'Append'},
+            {'class':'magic empty', 'value':'Text'},
+            {'class':'', 'value':'to'},
+            {'class':'magic empty', 'value':'Note'},
+        ])
+        compare_titles(self, 1, [
+            {'class':'', 'value':'Append'},
+            {'class':'magic', 'value':'Clipboard','glyph':'assets/cat/Clipboard.svg'},
+            {'class':'', 'value':'to'},
+            {'class':'magic', 'value':'Clipboard','glyph':'assets/cat/Clipboard.svg'},
+        ])
+        compare_titles(self, 2, [
+            {'class':'', 'value':'Append'},
+            {'class':'magic', 'value':'Text','glyph':'assets/cat/Ask.svg'},
+            {'class':'', 'value':'to'},
+            {'class':'magic', 'value':'Clipboard','glyph':'assets/cat/Clipboard.svg'},
+        ])
+        # Create note with X (in Y)
+        compare_titles(self, 3, [
+            {'class':'', 'value':'Create note with'},
+            {'class':'magic empty', 'value':'Body'},
+            {'class':'', 'value':'in'},
+            {'class':'magic empty', 'value':'Folder'},
+        ])
+        compare_titles(self, 4, [
+            {'class':'', 'value':'Create note with'},
+            {'class':'magic', 'value':'Clipboard','glyph':'assets/cat/Clipboard.svg'},
+            {'class':'', 'value':'in'},
+            {'class':'magic empty', 'value':'Folder'},
+        ])
+        compare_titles(self, 5, [
+            {'class':'', 'value':'Create note with'},
+            {'class':'magic', 'value':'Text','glyph':'assets/cat/Ask.svg'},
+            {'class':'', 'value':'in'},
+            {'class':'magic', 'value':'Folder','glyph':'assets/cat/Ask.svg'},
+        ])
+        compare_titles(self, 6, [
+            {'class':'', 'value':'Create note with'},
+            {'class':'magic empty', 'value':'Body'},
+        ])
+        # Show Note
+        compare_titles(self, 7, [
+            {'class':'', 'value':'Show'},
+            {'class':'magic empty', 'value':'Note'},
+        ])
+        compare_titles(self, 8, [
+            {'class':'', 'value':'Show'},
+            {'class':'magic', 'value':'Clipboard','glyph':'assets/cat/Clipboard.svg'},
+        ])
+        
---
+++ share/tests/test_batteries/test_dates.py
@@ -0,0 +1,51 @@
+## Dependency: sys
+from unittest import TestCase
+import logging
+
+
+## Dependency: battery testing
+from share.tests.test_batteries.battery import *
+
+
+class test_specify_date(TestCase):
+    reference_image = ''
+    actionID = '3fec82e3816e49f6a95236a3e85fdc1c'
+
+    # boilerplate
+
+    def setUp(self):
+        self.actions = actions_from_ID(__class__.actionID)
+
+    def tearDown(self):
+        del self.actions
+
+    def test_all_actions_loaded(self):
+        test_for_errors(self)
+        #test_no_not_implemented(self)
+        # temporarily exempt, as Ask Each Time produces no title elem
+
+    # Tailored Tests
+
+    def test_glyphs_correct(self):
+        [test_correct_glyph(self, action, 'Date.svg') for action in self.actions]
+
+    def test_categoryies_correct(self):
+        [self.assertEqual(action['category'], 'DATE') for action in self.actions]
+
+
+    def test_titles_correct(self):
+        compare_titles(self, 0, [
+            {'class':'magic', 'value':'Current Date'},
+        ])
+        compare_titles(self, 1, [
+            {'class':'magic', 'value':'Specified Date'},
+            {'class':'magic', 'value':'29th June 1700'},
+        ])
+        compare_titles(self, 2, [
+            {'class':'magic', 'value':'Specified Date'},
+            {'class':'magic empty', 'value':'29 June 2007'},
+        ])
+        compare_titles(self, 3, [
+            # no title elems
+            #TODO: test line elems
+        ])
