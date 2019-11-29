76e0b23dea4d47b1433c8f013cbcbfaa4dd25026
Elijah <emlyap99@gmail.com>
2019-11-30 07:51:56 +0800

    fix template error when disconnecting
    - dumps user back to home page,
    - I should add a message about successful disconnects
    - removed the messages, not sure what they were needed for

--- showcuts/user_util.py
+++ showcuts/user_util.py
@@ -1,20 +1,28 @@
-
+import logging
 ## Dependency: boilerplate
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def del_user(request, user, **kwargs):    
     try:
         u = User.objects.get(username = user.username)
         u.delete()
-        messages.success(request, "The user is deleted")            
+        logging.error("User deleted")#debug
+        #messages.success(request, "The user is deleted")            
 
     except User.DoesNotExist:
-        messages.error(request, "User doesnot exist")    
-        return render(request, 'submit')
+        logging.error("User not exist")#debug
+        #messages.error(request, "User does not exist")    
+        return redirect('/share/view')
+        #return render(request, 'logout')
 
     except Exception as e: 
-        return render(request, 'submit',{'err':e})
+        return redirect('/share/view')
 
-    return render(request, 'submit') 
+        #return render(request, 'logout',{'err':e})
+
+    # TODO: add a message
+    return redirect('/share/view')
