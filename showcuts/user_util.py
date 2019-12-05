49fb8893e21191a8385ddbeb566918706fd394b9
Elijah <emlyap99@gmail.com>
2019-12-05 19:48:48 +0800

    add reddit API code

--- showcuts/user_util.py
+++ showcuts/user_util.py
@@ -1,10 +1,17 @@
-import logging
+## Dependency: sys
+import logging, json
+from requests import post
+from requests.auth import HTTPBasicAuth
+
 ## Dependency: boilerplate
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse
+
+## local vars
+from showcuts.local_settings import REDDIT_CLIENT_ID, REDDIT_SECRET, REDDIT_URI
 
 def del_user(request, user, **kwargs):    
     try:
@@ -26,3 +33,50 @@ def del_user(request, user, **kwargs):
 
     # TODO: add a message
     return redirect('/share/view')
+
+def request_token(query:dict):
+    logging.error(query)
+    #TODO: check state to ensure it's legit
+    token_req = post(
+        url = 'https://www.reddit.com/api/v1/access_token',
+        auth = HTTPBasicAuth(REDDIT_CLIENT_ID, REDDIT_SECRET),
+        headers = {
+                "User-Agent": "Showcuts",
+        },
+        data = dict(
+            grant_type = 'authorization_code',
+            redirect_uri = REDDIT_URI,
+            code = query['code'],
+        ),
+    )
+    response = json.loads(token_req.content.decode('ASCII'))
+    
+    if 'error' in response: 
+        raise ValueError(response['error'])
+    if 'access_token' not in response:
+        raise ValueError('Could not log into Reddit')
+    return response['access_token']
+
+def post_reddit(token:str, subreddit:str, url:str, title:str):
+    logging.error(url)
+    reddit_post = post(
+        url='https://oauth.reddit.com/api/submit',
+        headers = {
+            "Authorization": f"bearer {token}", 
+            "User-Agent": "Showcuts",
+        },
+        data = dict(
+            sr = subreddit,
+            kind = 'link',
+            url = url,
+            title = title,
+        )
+    )
+    post_response = json.loads(reddit_post.content.decode('ASCII'))
+    try:
+        reddit_link = post_response['jquery'][16][3][0]
+        reddit_link = ('https://www.reddit.com' if 'reddit' not in reddit_link else '') + reddit_link
+        return reddit_link
+    except IndexError:
+        raise
+        raise ValueError('Failed to post link (might already be submitted, or account is rate limited)')
