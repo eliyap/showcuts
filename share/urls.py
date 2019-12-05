77180725e056b6a27b77345f089378a8315cb333
Elijah <emlyap99@gmail.com>
2019-12-05 19:50:03 +0800

    update URLs with new views organization

--- share/urls.py
+++ share/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
-from django.views.generic import RedirectView
-from share.views import *
+from share.views import submit, misc, display, user, xpost
 
 urlpatterns = [
-    path('', submit_iCloud, name='submit'),
-    path('submit/', submit_iCloud, name='submit'),
-    path('view',submit_iCloud, name='submit'),# could later become a browsing page
-    path('view/<str:hxid>',show_shortcut, name='view'),
-    path('submitted/', users_submitted.as_view(), name='user-submitted'),
-    path('liked/', users_liked.as_view(), name='user-liked'),
-    path('saved/', users_saved.as_view(), name='user-saved'),
-    path('error/', error, name='error'),
-    path('like/',like_shortcut, name='like-shortcut'),
-    path('save/',save_shortcut, name='save-shortcut'),
+    # aliases for home view
+    path('', submit.submit_iCloud, name='submit'),
+    path('submit/', submit.submit_iCloud, name='submit'),
+    path('view', submit.submit_iCloud, name='submit'),# could later become a browsing page
+    path('view/<str:hxid>',display.show_shortcut, name='view'),
+    
+    # for logged in users
+    path('submitted/', user.users_submitted.as_view(), name='user-submitted'),
+    path('liked/', user.users_liked.as_view(), name='user-liked'),
+    path('saved/', user.users_saved.as_view(), name='user-saved'),
+    
+    # share showcuts online
+    path('reddit/<str:hxid>', xpost.reddit, name = 'reddit'),
+
+    # misc paths
+    path('error/', misc.error, name='error'),
+    path('like/', display.like_shortcut, name='like-shortcut'),
+    path('save/', display.save_shortcut, name='save-shortcut'),
 ]
