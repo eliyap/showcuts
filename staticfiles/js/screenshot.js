2135bf0c4dbb01cc2c0cd375a4d408cea17eaef6
Elijah <emlyap99@gmail.com>
2019-12-06 16:29:42 +0800

    unhide  screenshot elements by defauly

--- staticfiles/js/screenshot.js
+++ staticfiles/js/screenshot.js
@@ -1,19 +1,19 @@
 
 $(document).ready(function () {
     $("#screenshot").click(function () {
-        $("#action-container").prepend($(".sc-title").clone().css({
-            'position': 'absolute',
-            'top': '1.5rem',
-            'left': '2rem',
-        }));
-        $("#screenshot-mark").css({'display':'block'});
+        // $("#action-container").prepend($(".sc-title").clone().css({
+        //     'position': 'absolute',
+        //     'top': '1.5rem',
+        //     'left': '2rem',
+        // }));
+        // $("#screenshot-mark").css({'display':'block'});
 
         domtoimage.toBlob(document.getElementById('action-container'))
             .then(function (blob) {
                 window.saveAs(blob, $(document).attr('title')+'.png');
             }).then(function(){
-                $("#action-container>.sc-title").remove();
-                $("#screenshot-mark").css({'display':'none'});
+                // $("#action-container>.sc-title").remove();
+                // $("#screenshot-mark").css({'display':'none'});
             });
 
     })
