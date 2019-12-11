e8be089947d511fc6eacc1cb8c557aff7db611ca
Elijah <emlyap99@gmail.com>
2019-12-11 11:16:30 +0800

    Update screenshot.js
    clean up comments

--- staticfiles/js/screenshot.js
+++ staticfiles/js/screenshot.js
@@ -1,20 +1,8 @@
-
 $(document).ready(function () {
     $("#screenshot").click(function () {
-        // $("#action-container").prepend($(".sc-title").clone().css({
-        //     'position': 'absolute',
-        //     'top': '1.5rem',
-        //     'left': '2rem',
-        // }));
-        // $("#screenshot-mark").css({'display':'block'});
-
-        domtoimage.toBlob(document.getElementById('action-container'))
+        domtoimage.toBlob(document.getElementById('action-container')) // cannot be turned into jQuery, I already tried.
             .then(function (blob) {
                 window.saveAs(blob, $(document).attr('title')+'.png');
-            }).then(function(){
-                // $("#action-container>.sc-title").remove();
-                // $("#screenshot-mark").css({'display':'none'});
             });
-
     })
 });
