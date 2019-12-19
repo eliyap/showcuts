61cd8da86325852fb00dce9315b0448a5edf277b
Elijah <emlyap99@gmail.com>
2019-12-19 07:11:58 -0800

    Update custom.js
    - add generic clicked / not clicked handler
    NOTE: some scripts cannot farm out click toggling, as they rely on the logic being integrated

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -1,10 +1,9 @@
-
 $(document).ready(function () {
     /* Set sidenav margin to 0px */
     function openNav() {
         $("#mySidenav").removeClass("closed").addClass("opened");
     }
-    
+
     /* Set sidenav width to -250px */
     function closeNav() {
         $("#mySidenav").removeClass("opened").addClass("closed");
@@ -14,15 +13,13 @@ $(document).ready(function () {
     $("#open-fixed").click(openNav)
     $("#close").click(closeNav)
 
-    // Store references outside event handler:
     let $window = $(window);
-    let $sidebar = $(mySidenav);
-    
+
     function checkWidth() {
         let windowsize = $window.width();
         if ($("#mySidenav").hasClass("hidden")) {
-            $("#open").addClass("always-on")
-            $("#close").addClass("always-on")
+            $("#open").addClass("always-on");
+            $("#close").addClass("always-on");
         }
         else {
             if (windowsize > 768) {
@@ -36,106 +33,104 @@ $(document).ready(function () {
         }
     }
     checkWidth();// Execute on load
-    $(window).resize(checkWidth)// Bind event listener
-    $("#id_iCloudLink").focus()// place cursor (on form page)
-    $("#id_subreddit").focus()// place cursor (on form page)
+    $(window).resize(checkWidth);// Bind event listener
+    $("#id_iCloudLink").focus();// place cursor (on form page)
+    $("#id_subreddit").focus();// place cursor (on form page)
 
 
     // Magic Variable Toggle
     let output_shown = false;
-    function toggleOutput(){
-        if (output_shown){
+
+    $("#show-magic").click(function toggleOutput() {
+        if (output_shown) {
             $(".output").removeClass("shown").addClass("hidden");
             output_shown = false;
         }
-        else{
+        else {
             $(".output").removeClass("hidden").addClass("shown");
             output_shown = true;
         }
-    }
-    
-    $("#show-magic").click(toggleOutput)
-    
+    });
+
     // Like Button AJAX
-    function toggleLike(){
+    function toggleLike() {
         let elem = $(this);
         // alert('hi!');
-        if (!elem.hasClass('disabled')){
-            let likes = elem.next();
-            let num_likes = parseInt(likes.text());
-            let hxid = elem.attr("hxid");
-            $.ajax({
-                type:"GET",
-                url:window.location.origin+"/share/like",
-                data:{
-                    hxid: hxid,
-                },
-                success: function (){
-                    if (elem.hasClass("clicked")){
-                        elem.removeClass("clicked");
-                        likes.text(num_likes - 1);
-                    }
-                    else{
-                        elem.addClass("clicked");
-                        likes.text(num_likes + 1);
-                    }
+        let likes = elem.next();
+        let num_likes = parseInt(likes.text());
+        let hxid = elem.attr("hxid");
+        $.ajax({
+            type: "GET",
+            url: window.location.origin + "/share/like",
+            data: {
+                hxid: hxid,
+            },
+            success: function () {
+                if (elem.hasClass("clicked")) {
+                    elem.removeClass("clicked");
+                    likes.text(num_likes - 1);
                 }
-            });
-        }
+                else {
+                    elem.addClass("clicked");
+                    likes.text(num_likes + 1);
+                }
+            }
+        });
     }
     $("#like").unbind('click').click(toggleLike)
     $(".like-btn").unbind('click').click(toggleLike)
     // Save Button AJAX
-    $("#save").unbind('click').click(function toggleSave(){
+    $("#save").unbind('click').click(function toggleSave() {
         let elem = $(this);
-        if (!elem.hasClass('disabled')){
-            let hxid = elem.attr("hxid");
-            $.ajax({
-                type:"GET",
-                url:window.location.origin+"/share/save",
-                data:{
-                    hxid: hxid,
-                },
-                success: function (){
-                    if (elem.hasClass("clicked")){
-                        elem.removeClass("clicked");
-                    }
-                    else{
-                        elem.addClass("clicked");
-                    }
+        let hxid = elem.attr("hxid");
+        $.ajax({
+            type: "GET",
+            url: window.location.origin + "/share/save",
+            data: {
+                hxid: hxid,
+            },
+            success: function () {
+                if (elem.hasClass("clicked")) {
+                    elem.removeClass("clicked");
                 }
-            });
-        }
+                else {
+                    elem.addClass("clicked");
+                }
+            }
+        });
     })
-    $("#xpost,#info").unbind('click').click(function (){
+    $("#xpost,#info").unbind('click').click(function () {
         let elem = $(this);
-        if (elem.hasClass("clicked")){
+        if (elem.hasClass("clicked")) {
             elem.removeClass("clicked");
         }
-        else{
+        else {
             elem.addClass("clicked");
         }
     });
-    // account deleting warning
-    $("#safety-glass").unbind('click').click(function (){
-        let elem=$(this);
+
+    // account deletion warning
+    $("#safety-glass").unbind('click').click(function () {
+        let elem = $(this);
         $("#disconnect-warning").addClass("show");
         $("h1,h2,h3,h4,h5,h6,p,form").addClass("blurred");
         elem.hide();
     });
 
-    $("#accept-delete").unbind('click').click(function (){
+    $("#accept-delete").unbind('click').click(function () {
         $("#disconnect-warning").removeClass("show");
         $(".blurred").removeClass("blurred");
     });
-    
-    $("#icloud-submit").click(function (){
+
+    $("#icloud-submit").click(function () {
         $(this).addClass("clicked");
     })
 });
-$(window).bind('beforeunload', function(){
-    if ($("#icloud-submit").hasClass('clicked')){
-        $("#loading").css({'display':'block','opacity':'100%'});
+
+// loading animation
+$(window).bind('beforeunload', function () {
+    if ($("#icloud-submit").hasClass('clicked')) {
+        $("#loading").css({ 'display': 'block', 'opacity': '100%' });
     }
 });
-  
+
