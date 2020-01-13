220315d4d75102af7f7a3d3e3d651bf044941621
Elijah <emlyap99@gmail.com>
2020-01-12 20:03:50 -0800

    Update music.py
    add support for new actions

--- share/process/sc_action/categories/music.py
+++ share/process/sc_action/categories/music.py
@@ -1,3 +1,5 @@
+from share.process.components._directory import *
+from ..action import *
 from ..action import *
 
 class _base(action):
@@ -10,17 +12,86 @@ class getcurrentsong(_base):
 
 class playmusic(_base):
     name = 'Play Music'
+    title = [
+        text('Play'),
+        music(
+            'WFMediaItems',
+            blank_text='Music',
+            ask_each_time='Music',
+        ),
+    ]
+    lines = [
+        line_pill(
+            'Shuffle',
+            'WFPlayMusicActionShuffle',
+            ask_each_time='Ask Each Time',
+            default='', # none selected,
+            options=[
+                'Off',
+                'Songs',
+            ],
+        ),
+        line_pill(
+            'Repeat',
+            'WFPlayMusicActionRepeat',
+            ask_each_time='Ask Each Time',
+            default='', # none selected,
+            options=[
+                'None',
+                'One',
+                'All',
+            ],
+        ),
+    ]
     pass
 
 class pausemusic(_base):
     name = 'Play/Pause'
     glyph = 'Play-Pause.svg'
     category = 'NOW PLAYING'
+    title = [
+        choose(
+            'WFPlayPauseBehavior',
+            ask_each_time='Ask Each Time',
+            default='Play/Pause',
+            options=[
+                'Play/Pause',
+                'Play',
+                'Pause',
+            ],
+        ),
+        text('on'),
+        choose(
+            'WFMediaRoute',
+            ask_each_time='Device',
+            default='iPhone', # TODO: test with other devices, esp HomePods!
+            options=['iPhone'],
+        ),
+    ]
 
 class skipback(_base):
     name = 'Skip Back'
     glyph = 'Skip-Back.svg'
     category = 'NOW PLAYING'
+    title = [
+        text('Skip back to the'),
+        choose(
+            'WFSkipBackBehavior',
+            ask_each_time='Skip To',
+            default='Beginning',
+            options=[
+                'Beginning',
+                'Previous Song',
+            ],
+        ),
+        text('on'),
+        choose(
+            'WFMediaRoute',
+            ask_each_time='Device',
+            default='iPhone', # TODO: test with other devices, esp HomePods!
+            options=['iPhone'],
+        ),
+    ]
 
 class skipforward(_base):
     name = 'Skip Forward'
@@ -29,10 +100,36 @@ class skipforward(_base):
 
 class exportsong(_base):
     name = 'Select Music'
+    title = [
+        text('Select music'),
+    ]
+    lines = [
+        line_toggle(
+            'Select Multiple Songs',
+            'WFExportSongActionSelectMultiple',
+            default = False,
+            ask_each_time = 'Ask Each Time',
+        )
+    ]
     result = 'Music'
 
 class addtoplaylist(_base):
     name = 'Add to Playlist'
+    title = [
+        text('Add'),
+        magic(
+            'WFInput',
+            blank_text='Music',
+            ask_each_time=None,
+        ),
+        text('to'),
+        choose(
+            'WFPlaylistName',
+            ask_each_time='Playlist',
+            default='My Music Library',
+            options=['My Music Library'], # dependent on device
+        ),
+    ]
     result = 'Updated Playlist'
 
 class createplaylist(_base):
@@ -45,7 +142,27 @@ class get_playlist(_base):
 
 class addmusictoupnext(_base):
     name = 'Add to Up Next'
+    title = [
+        text('Add'),
+        music(
+            'WFMusic',
+            blank_text='Music',
+            ask_each_time='Music',
+        ),
+        text('to'),
+        choose(
+            'WFWhenToPlay',
+            ask_each_time='Play',
+            default='Next',
+            options=[
+                'Next',
+                'Later',
+            ],
+        ),
+        text('of Up Next'),
+    ]
 
 class clearupnext(_base):
     name = 'Clear Up Next'
+    title = [text('Clear Up Next')]
     
