9533b710446a77bdfe469fec70c8df42452d8ae9
Elijah <emlyap99@gmail.com>
2020-01-15 12:04:13 -0800

    Update date.py
    - add date support

--- share/process/sc_action/categories/date.py
+++ share/process/sc_action/categories/date.py
@@ -1,3 +1,5 @@
+from share.process.components._directory import *
+from ..action import *
 from ..action import *
 
 class _base(action):
@@ -7,6 +9,48 @@ class _base(action):
 class date(_base):
     name = 'Date'
     category = 'DATE'
+    title = [
+        choose(
+            'WFDateActionMode',
+            ask_each_time='Ask Each Time',
+            default='Current Date',
+            options=[
+                'Current Date',
+                'Specified Date',
+            ],
+        ),
+        inline(
+            'WFDateActionDate',
+            blank_text='29 June 2007',
+            ask_each_time='Date',
+        ),
+    ]
+    lines = [
+        line_choose(
+            'Use',
+            'WFDateActionMode',
+            ask_each_time='Ask Each Time',
+            default='Current Date',
+            options=[
+                'Current Date',
+                'Specified Date',
+            ],
+        ),
+    ]
+    def modify(self):
+        {
+            'Current Date': lambda: [
+                self.hide_title('WFDateActionDate'),
+                self.hide_line('WFDateActionMode'),
+            ],
+            'Specified Date': lambda: self.hide_line('WFDateActionMode'),
+        }.get(
+            self.get_title('WFDateActionMode')['value'],
+            lambda:[ # magic variable
+                self.hide_title('WFDateActionMode'),
+                self.hide_title('WFDateActionDate'),
+            ],
+        )()
     result = 'Date'
 
 class format_date(_base):
