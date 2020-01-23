f1350207179ee4d208d540a7f7bb35f8c0bc3a87
Elijah <emlyap99@gmail.com>
2020-01-23 11:43:20 -0800

    Update _directory.py
    rename text element

--- share/process/components/_directory.py
+++ share/process/components/_directory.py
@@ -1,4 +1,4 @@
-from .text import elem as text
+from .text import text
 from .title.magic import magic
 from .title.choose import choose
 from .title.inline import inline, list_inline
@@ -6,8 +6,11 @@ from .title.counter import counter
 from .title.number import number, whole_number
 from .title.unit import unit, long_unit
 from .title.music import music
+from .title.location import location
+from .title.quantity import quantity_magnitude, quantity_unit
 
 from .base_line import line
+from .line.magic import line_magic
 from .line.choose import line_choose
 from .line.inline import line_inline
 from .line.toggle import line_toggle
